82db37da972be8b5700edfbb4fe7a67a5dfd387c
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-02-14 10:31:01 -0800

    completed exercise 21.

---
+++ car_wheels.py
@@ -0,0 +1,62 @@
+# Kami Bigdely
+# Move Field
+
+class Car:
+    def __init__(self, engine, wheels, cabin, fuel_tank):
+        self.engine = engine
+        self.wheels = wheels
+        for w in wheels:
+            w.set_car(self)
+        self.cabin = cabin
+        self.fuel_tank = fuel_tank
+
+class Wheel:
+    def __init__(self, car=None, wheel_location=None, tpms=None):
+        self.car = car
+        self.wheel_location = wheel_location
+        self.tpms = tpms
+    
+    def install_tire(self):
+        print('remove old tube.')
+        print('cleaned tpms: ', self.tpms.get_serial_number(), '.')
+        print('installed new tube.')        
+        
+    def read_tire_pressure(self):
+        return self.tpms.get_pressure()
+    
+    def set_car(self, car):
+        self.car = car
+
+class Tpms:
+    """Tire Pressure Monitoring System."""
+    def __init__(self, serial_number):
+        self.serial_number = serial_number
+        self.sensor_transmit_range = 300  # [feet]
+        self.sensor_pressure_range = (8, 300)  # [PSI]
+        self.battery_life = 6  # [year]
+        
+    def get_pressure(self):
+        return 3
+    
+    def get_serial_number(self):
+        return self.serial_number
+    
+class Engine:
+    pass
+    
+class FuelTank:
+    pass
+    
+class Cabin:
+    pass    
+
+engine = Engine()
+wheels = [Wheel(None, 'front-right', Tpms(983408543)), 
+          Wheel(None, 'front-left', Tpms(4343083)), 
+          Wheel(None, 'back-right', Tpms(23654835)), 
+          Wheel(None, 'back-left', Tpms(3498857))]
+
+cabin  = Cabin()
+fuel_tank = FuelTank()
+
+my_car = Car(engine, wheels, cabin, fuel_tank)
