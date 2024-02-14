5d084eedf18ab46835636346fa87363e2a057489
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-02-14 10:37:38 -0800

    completed all exercises.

--- draw_shapes.py
+++ draw_shapes.py
@@ -1,37 +1,15 @@
 # by Kami Bigdely
 # Extract superclass.
-class Circle:
-    
-    def __init__(self, x, y, r, visible = True):
-      self.center_x = x
-      self.center_y = y
-      self.r = r
-      self.visible = visible
-      
-    def display(self):
-        print('drew the circle.')
-        
-    def set_visible(self,is_visible):
-        self.visible = is_visible
-        
-    def get_center(self):
-        return self.center_x, self.center_y
-    
-    
-class Rectangle:
-    
-    def __init__(self, x, y, width, height, visible = True):
-        # left-bottom corner.
+class Shape:
+    def __init__(self, x, y, visible=True):
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
         self.visible = visible
         
     def display(self):
         if self.visible:
-            print('drew the rectable.')
-            
+            print('Drew the shape.')
+        
     def hide(self):
         self.visible = False
         
@@ -39,20 +17,42 @@ class Rectangle:
         self.visible = True
         
     def get_center(self):
-        return self.x + self.width/2, \
-               self.y + self.height/2 
+        raise NotImplementedError("Subclasses must implement get_center method.")
 
-
+class Circle(Shape):
+    def __init__(self, x, y, r, visible=True):
+        super().__init__(x, y, visible)
+        self.r = r
+    
+    def display(self):
+        super().display()
+        print('Drew the circle.')
+        
+    def get_center(self):
+        return self.x, self.y
+    
+class Rectangle(Shape):
+    def __init__(self, x, y, width, height, visible=True):
+        super().__init__(x, y, visible)
+        self.width = width
+        self.height = height
+    
+    def display(self):
+        super().display()
+        print('Drew the rectangle.')
+        
+    def get_center(self):
+        return self.x + self.width/2, self.y + self.height/2
 
 if __name__ == "__main__":
-    circle = Circle(0,0,10, False)
-    circle.set_visible(True)
+    circle = Circle(0, 0, 10, False)
+    circle.make_visible()
     circle.display()
-    print('center point',circle.get_center())
+    print('Center point:', circle.get_center())
 
     rect = Rectangle(10, 10, 20, 5)
     rect.hide()
-    rect.display() # does not display because it's hidden.
+    rect.display()  # does not display because it's hidden.
     rect.make_visible()
     rect.display()
-    print('center point',rect.get_center())
+    print('Center point:', rect.get_center())
