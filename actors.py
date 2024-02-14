8de13af6cdac9cb8be12e9c9637cc11e251e844a
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-02-14 10:34:05 -0800

    completed exercise 23.

--- actors.py
+++ actors.py
@@ -1,20 +1,25 @@
 # by Kami Bigdely
 # Extract class
-first_names = ['elizabeth', 'Jim']
-last_names = ['debicki', 'Carrey']
-birth_year = [1990, 1962]
-movies = [['Tenet', 'Vita & Virgina', 'Guardians of the Galexy', 'The Great Gatsby'],\
-     ['Ace Ventura', 'The Mask', 'Dubm and Dumber', 'The Truman Show', 'Yes Man']]
-emails = ['deb@makeschool.com', 'jim@makeschool.com']
+class Actor:
+    def __init__(self, first_name, last_name, birth_year, movies, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birth_year = birth_year
+        self.movies = movies
+        self.email = email
 
-def send_hiring_email(email):
-    print("email sent to: ", email)
-    
-for i, value in enumerate(emails):
-    if birth_year[i] > 1985:
-        print(first_names[i], last_names[i])
-        print('Movies Played: ', end='')
-        for m in movies[i]:
-            print(m, end=', ')
-        print()
-        send_hiring_email(value)
+    def send_hiring_email(self):
+        print("Email sent to:", self.email)
+
+actors = [
+    Actor('Elizabeth', 'Debicki', 1990, ['Tenet', 'Vita & Virgina', 'Guardians of the Galaxy', 'The Great Gatsby'],
+          'deb@makeschool.com'),
+    Actor('Jim', 'Carrey', 1962, ['Ace Ventura', 'The Mask', 'Dumb and Dumber', 'The Truman Show', 'Yes Man'],
+          'jim@makeschool.com')
+]
+
+for actor in actors:
+    if actor.birth_year > 1985:
+        print(actor.first_name, actor.last_name)
+        print('Movies Played:', ', '.join(actor.movies))
+        actor.send_hiring_email()
