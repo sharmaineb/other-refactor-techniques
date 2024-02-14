639ef8d18d17b43de90059853103c81fca6a1de6
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-02-14 10:35:20 -0800

    completed exercise 24.

--- steaks_revisited.py
+++ steaks_revisited.py
@@ -1,35 +1,36 @@
 # by Kami Bigdely
 # Extract class
-WELL_DONE = 3000
-MEDIUM = 2500
-COOKED_CONSTANT = 0.05
+class Steak:
+    WELL_DONE = 3000
+    MEDIUM = 2500
+    COOKED_CONSTANT = 0.05
+
+    def __init__(self, time, temperature, pressure):
+        self.time = time
+        self.temperature = temperature
+        self.pressure = pressure
+
+    def is_cooked_to_criteria(self, desired_state):
+        return self.is_well_done(desired_state) or self.is_medium(desired_state)
+
+    def is_well_done(self, desired_state):
+        return desired_state == 'well-done' and self.get_cooking_progress() >= self.WELL_DONE
+
+    def is_medium(self, desired_state):
+        return desired_state == 'medium' and self.get_cooking_progress() >= self.MEDIUM
+
+    def get_cooking_progress(self):
+        return self.time * self.temperature * self.pressure * self.COOKED_CONSTANT
 
 
-def is_cookeding_criteria_satisfied(time, temperature, 
-                                    pressure, desired_state):
-    return is_well_done(time, temperature, pressure, desired_state) or \
-           is_medium(time, temperature, pressure, desired_state)
-
-
-def is_well_done(time, temperature, pressure, desired_state):    
-    return desired_state == 'well-done' and  \
-           get_cooking_progress(time, temperature, pressure) >= WELL_DONE
-
-
-def is_medium(time, temperature, pressure, desired_state):
-    return desired_state == 'medium' and  \
-           get_cooking_progress(time, temperature, pressure) >= MEDIUM
-
-def get_cooking_progress(time, temperature, pressure):
-    return time * temperature * pressure * COOKED_CONSTANT
-
-
-time = 30 # [min]
-temp = 103 # [celcius]
-pressure = 20 # [psi]
+time = 30  # [min]
+temp = 103  # [celcius]
+pressure = 20  # [psi]
 desired_state = 'well-done'
 
-if is_cookeding_criteria_satisfied(time, temp, pressure, desired_state):
-    print('cooking is done.')
+steak = Steak(time, temp, pressure)
+
+if steak.is_cooked_to_criteria(desired_state):
+    print('Cooking is done.')
 else:
-    print('ongoing cooking.')
+    print('Ongoing cooking.')
